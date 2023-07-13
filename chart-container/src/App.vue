0143ba5d90972a9b54a9021e54b1a705dd84f39d
maynion <mcarnecer@startechup.com>
2023-07-13 12:43:55 +0800

    fixes on chart data

--- chart-container/src/App.vue
+++ chart-container/src/App.vue
@@ -1,112 +1,28 @@
 <template>
-  <div style="width: 100%">
-    <div class="content">
-      <grid-layout
-        :layout="selectedCharts"
-        :col-num="12"
-        :is-draggable="true"
-        :is-resizable="true"
-        :vertical-compact="true"
-        :use-css-transforms="true"
+  <div>
+    <grid-layout
+      :layout="selectedCharts"
+      :col-num="12"
+      :is-draggable="true"
+      :is-resizable="true"
+      :vertical-compact="true"
+      :use-css-transforms="true"
+    >
+      <grid-item
+        v-for="item in selectedCharts"
+        :key="item.i"
+        :x="item.x"
+        :y="item.y"
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
       >
-        <grid-item
-          v-for="item in selectedCharts"
-          :key="item.i"
-          :x="item.x"
-          :y="item.y"
-          :w="item.w"
-          :h="item.h"
-          :i="item.i"
+        <ChartData :chartType="item.chart.value" :chartLib="item.selectedLib" />
+        <span class="remove" @click="removeItem(item.i)"
+          ><v-icon size="small">mdi-close</v-icon></span
         >
-          <ChartData
-            :chartType="item.chart.value"
-            :chartLib="item.selectedLib"
-          />
-          <span class="remove" @click="removeItem(item.i)"
-            ><v-icon size="small">mdi-close</v-icon></span
-          >
-          <div class="custom-toolbox">
-            <v-icon color="#676767" @click="editDialog = !editDialog"
-              >mdi-pencil-outline</v-icon
-            >
-            <v-icon color="#676767">mdi-download-outline</v-icon>
-            <v-icon color="#676767" @click="embedDialog = !embedDialog"
-              >mdi-import</v-icon
-            >
-          </div>
-        </grid-item>
-      </grid-layout>
-    </div>
-  </div>
-  <div class="toolbox-dialog">
-    <v-dialog v-model="editDialog" width="500px">
-      <v-card>
-        <v-card-text>
-          <v-row justify="space-between">
-            <v-col cols="6">
-              <v-sheet class="my-2"><h3>Parameter Area</h3></v-sheet>
-            </v-col>
-            <v-col cols="1">
-              <v-sheet class="my-2"
-                ><v-icon @click="editDialog = !editDialog"
-                  >mdi-close</v-icon
-                ></v-sheet
-              >
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <p class="text-center mb-2">x-Axis</p>
-              <v-select
-                v-model="xAxisData"
-                label="Select x-axis data"
-                :items="xCategories"
-                variant="solo"
-                @update:modelValue="selectedXaxisData"
-              ></v-select>
-            </v-col>
-            <v-col>
-              <p class="text-center mb-2">y-Axis</p>
-              <v-select
-                v-model="yAxisData"
-                label="Select y-axis data"
-                :items="yCategories"
-                variant="solo"
-                @update:modelValue="selectedYaxisData"
-              ></v-select>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-
-    <v-dialog v-model="embedDialog" width="500px">
-      <v-card>
-        <v-card-text>
-          <v-row justify="space-between">
-            <v-col cols="8">
-              <v-sheet class="my-2"
-                ><h3>Add the widget to your website</h3>
-              </v-sheet>
-            </v-col>
-            <v-col cols="1">
-              <v-sheet class="my-2"
-                ><v-icon @click="embedDialog = !embedDialog"
-                  >mdi-close</v-icon
-                ></v-sheet
-              >
-            </v-col>
-          </v-row>
-          <v-textarea
-            id="tocopy"
-            variant="outlined"
-            density="compact"
-            append-inner-icon="mdi-content-copy"
-            @click:append-inner="copyText"
-          ></v-textarea>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+      </grid-item>
+    </grid-layout>
   </div>
 </template>
 
@@ -117,155 +33,20 @@ import { storeToRefs } from "pinia";
 import { useStore } from "../../dashboard/src/stores/selectedChartItems";
 const store = useStore();
 const { selectedCharts } = storeToRefs(store);
-const editDialog = ref(false);
-const embedDialog = ref(false);
-const xAxisData = ref([]);
-const yAxisData = ref([]);
-const xCategories = ref(["Days", "Number", "Category", "Time"]);
-const yCategories = ref([
-  "Default",
-  "Days",
-  "Precipitation",
-  "Temperature",
-  "Category",
-]);
 
 // functions
 function removeItem(i) {
   const index = this.selectedCharts.map((item) => item.i).indexOf(i);
   this.selectedCharts.splice(index, 1);
 }
-
-function selectedXaxisData(x) {
-  if (x === "Days") {
-    const daysItem = {
-      categories: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-      values: {
-        data: [60, 35, 110, 80, 95, 140, 185],
-      },
-    };
-    this.selectedCharts.xAxis.data = daysItem.categories;
-    this.selectedCharts.series = daysItem.values;
-  } else if (x === "Number") {
-    const numberItem = {
-      categories: [1, 2, 3, 4, 5],
-      values: {
-        data: [50, 100, 150, 200, 250],
-      },
-    };
-    this.selectedCharts.xAxis.data = numberItem.categories;
-    this.selectedCharts.series = numberItem.values;
-  } else if (x === "Category") {
-    const categoryItem = [
-      "Direct",
-      "Email",
-      "Ad Networks",
-      "Video Ads",
-      "Search Engines",
-    ];
-    this.selectedCharts.xAxis.data = categoryItem;
-  } else if (x === "Time") {
-    const timeItem = {
-      categories: [10, 40, 70, 100, 130],
-      values: {
-        data: [55, 75, 95, 115, 135],
-      },
-    };
-    this.selectedCharts.xAxis.data = timeItem.categories;
-    this.selectedCharts.series = timeItem.values;
-  }
-}
-
-function selectedYaxisData(y) {
-  if (y === "Default") {
-    const defaultItem = {
-      type: "value",
-      min: 0,
-      max: 200,
-      interval: 50,
-      axisLabel: {
-        formatter: "{value}",
-      },
-    };
-    this.selectedCharts.yAxis = defaultItem;
-  } else if (y === "Days") {
-    const daysItem = {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    };
-    this.selectedCharts.yAxis = daysItem;
-  } else if (y === "Precipitation") {
-    const precipitationItem = {
-      type: "value",
-      name: "Precipitation",
-      min: 0,
-      max: 250,
-      interval: 50,
-      axisLabel: {
-        formatter: "{value} ml",
-      },
-    };
-    this.selectedCharts.yAxis = precipitationItem;
-  } else if (y === "Temperature") {
-    const tempItem = {
-      type: "value",
-      name: "Temperature",
-      min: 0,
-      max: 25,
-      interval: 5,
-      axisLabel: {
-        formatter: "{value} °C",
-      },
-    };
-    this.selectedCharts.yAxis = tempItem;
-  } else if (y === "Category") {
-    const categoryItem = {
-      type: "category",
-      data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"],
-    };
-    this.selectedCharts.yAxis = categoryItem;
-  }
-}
-
-function copyText() {
-  const input = document.getElementById("tocopy");
-  input.select();
-  document.execCommand("copy");
-}
 </script>
 
 <style scoped>
-.content {
-  width: 100%;
-}
-
 .layoutJSON {
   background: #ddd;
   border: 1px solid black;
   margin-top: 10px;
   padding: 10px;
-}
-
-.columns {
-  -moz-columns: 120px;
-  -webkit-columns: 120px;
-  columns: 120px;
-}
-
-.vue-resizable-handle {
-  z-index: 5000;
-  position: absolute;
-  width: 20px;
-  height: 20px;
-  bottom: 0;
-  right: 0;
-  background: url("data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/Pg08IS0tIEdlbmVyYXRvcjogQWRvYmUgRmlyZXdvcmtzIENTNiwgRXhwb3J0IFNWRyBFeHRlbnNpb24gYnkgQWFyb24gQmVhbGwgKGh0dHA6Ly9maXJld29ya3MuYWJlYWxsLmNvbSkgLiBWZXJzaW9uOiAwLjYuMSAgLS0+DTwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+DTxzdmcgaWQ9IlVudGl0bGVkLVBhZ2UlMjAxIiB2aWV3Qm94PSIwIDAgNiA2IiBzdHlsZT0iYmFja2dyb3VuZC1jb2xvcjojZmZmZmZmMDAiIHZlcnNpb249IjEuMSINCXhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHhtbDpzcGFjZT0icHJlc2VydmUiDQl4PSIwcHgiIHk9IjBweCIgd2lkdGg9IjZweCIgaGVpZ2h0PSI2cHgiDT4NCTxnIG9wYWNpdHk9IjAuMzAyIj4NCQk8cGF0aCBkPSJNIDYgNiBMIDAgNiBMIDAgNC4yIEwgNCA0LjIgTCA0LjIgNC4yIEwgNC4yIDAgTCA2IDAgTCA2IDYgTCA2IDYgWiIgZmlsbD0iIzAwMDAwMCIvPg0JPC9nPg08L3N2Zz4=");
-  background-position: bottom right;
-  padding: 0 3px 3px 0;
-  background-repeat: no-repeat;
-  background-origin: content-box;
-  box-sizing: border-box;
-  cursor: se-resize;
 }
 
 .vue-grid-item:not(.vue-grid-placeholder) {
@@ -278,41 +59,10 @@ function copyText() {
   opacity: 0.9;
 }
 
-.vue-grid-item.static {
-  background: #cce;
-}
-
-.vue-grid-item .text {
-  font-size: 24px;
-  text-align: center;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  height: 24px;
-}
-
-.vue-grid-item .minMax {
-  font-size: 12px;
-}
-
-.vue-grid-item .add {
-  cursor: pointer;
-}
-
 .remove {
   position: absolute;
   right: 2px;
   top: 0;
-  cursor: pointer;
-}
-
-.custom-toolbox {
-  position: absolute;
-  right: 2px;
-  top: 30px;
   cursor: pointer;
 }
 </style>
--- chart-container/src/components/charts/ChartData.vue
+++ chart-container/src/components/charts/ChartData.vue
@@ -2,6 +2,85 @@
   <EChart v-if="chartLib === 'eCharts'" :option="options" />
   <ApexCharts v-if="chartLib === 'apexCharts'" />
   <ChartJS v-if="chartLib === 'chartjs'" />
+  <div class="custom-toolbox">
+    <v-icon color="#676767" @click="editDialog = !editDialog"
+      >mdi-pencil-outline</v-icon
+    >
+    <v-icon color="#676767">mdi-download-outline</v-icon>
+    <v-icon color="#676767" @click="embedDialog = !embedDialog"
+      >mdi-import</v-icon
+    >
+  </div>
+  <div class="toolbox-dialog">
+    <v-dialog v-model="editDialog" persistent width="500px" style="z-index: 0">
+      <v-card>
+        <v-card-text>
+          <v-row justify="space-between">
+            <v-col cols="6">
+              <v-sheet class="my-2"><h3>Parameter Area</h3></v-sheet>
+            </v-col>
+            <v-col cols="1">
+              <v-sheet class="my-2"
+                ><v-icon @click="editDialog = !editDialog"
+                  >mdi-close</v-icon
+                ></v-sheet
+              >
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <p class="text-center mb-2">x-Axis</p>
+              <v-select
+                v-model="xAxisData"
+                :items="xCategories"
+                label="Select x-axis data"
+                variant="solo"
+                @update:modelValue="selectedXaxisData"
+              ></v-select>
+            </v-col>
+            <v-col>
+              <p class="text-center mb-2">y-Axis</p>
+              <v-select
+                v-model="yAxisData"
+                :items="yCategories"
+                label="Select y-axis data"
+                variant="solo"
+                @update:modelValue="selectedYaxisData"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="embedDialog" width="500px" style="z-index: 0">
+      <v-card>
+        <v-card-text>
+          <v-row justify="space-between">
+            <v-col cols="8">
+              <v-sheet class="my-2"
+                ><h3>Add the widget to your website</h3>
+              </v-sheet>
+            </v-col>
+            <v-col cols="1">
+              <v-sheet class="my-2"
+                ><v-icon @click="embedDialog = !embedDialog"
+                  >mdi-close</v-icon
+                ></v-sheet
+              >
+            </v-col>
+          </v-row>
+          <v-textarea
+            id="tocopy"
+            variant="outlined"
+            density="compact"
+            append-inner-icon="mdi-content-copy"
+            @click:append-inner="copyText"
+          ></v-textarea>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
@@ -23,6 +102,18 @@ export default {
   data: () => {
     return {
       options: null,
+      editDialog: false,
+      embedDialog: false,
+      xAxisData: [],
+      xCategories: ["Days", "Number", "Category", "Time"],
+      yAxisData: [],
+      yCategories: [
+        "Default",
+        "Days",
+        "Precipitation",
+        "Temperature",
+        "Category",
+      ],
     };
   },
   mounted() {
@@ -53,6 +144,118 @@ export default {
       };
       store.getChartOptions(this.options);
     },
+
+    copyText() {
+      const input = document.getElementById("tocopy");
+      input.select();
+      document.execCommand("copy");
+    },
+
+    selectedXaxisData(x) {
+      if (x === "Days") {
+        const daysItem = {
+          categories: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          values: {
+            data: [60, 35, 110, 80, 95, 140, 185],
+          },
+        };
+        this.options.xAxis.data = daysItem.categories;
+        this.options.series = daysItem.values;
+      } else if (x === "Number") {
+        const numberItem = {
+          categories: [1, 2, 3, 4, 5],
+          values: {
+            data: [50, 100, 150, 200, 250],
+          },
+        };
+        this.options.xAxis.data = numberItem.categories;
+        this.options.series = numberItem.values;
+      } else if (x === "Category") {
+        const categoryItem = [
+          "Direct",
+          "Email",
+          "Ad Networks",
+          "Video Ads",
+          "Search Engines",
+        ];
+        this.options.xAxis.data = categoryItem;
+      } else if (x === "Time") {
+        const timeItem = {
+          categories: [10, 40, 70, 100, 130],
+          values: {
+            data: [55, 75, 95, 115, 135],
+          },
+        };
+        this.options.xAxis.data = timeItem.categories;
+        this.options.series = timeItem.values;
+      }
+    },
+
+    selectedYaxisData(y) {
+      if (y === "Default") {
+        const defaultItem = {
+          type: "value",
+          min: 0,
+          max: 200,
+          interval: 50,
+          axisLabel: {
+            formatter: "{value}",
+          },
+        };
+        this.options.yAxis = defaultItem;
+      } else if (y === "Days") {
+        const daysItem = {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+        };
+        this.options.yAxis = daysItem;
+      } else if (y === "Precipitation") {
+        const precipitationItem = {
+          type: "value",
+          name: "Precipitation",
+          min: 0,
+          max: 250,
+          interval: 50,
+          axisLabel: {
+            formatter: "{value} ml",
+          },
+        };
+        this.options.yAxis = precipitationItem;
+      } else if (y === "Temperature") {
+        const tempItem = {
+          type: "value",
+          name: "Temperature",
+          min: 0,
+          max: 25,
+          interval: 5,
+          axisLabel: {
+            formatter: "{value} °C",
+          },
+        };
+        this.options.yAxis = tempItem;
+      } else if (y === "Category") {
+        const categoryItem = {
+          type: "category",
+          data: [
+            "Direct",
+            "Email",
+            "Ad Networks",
+            "Video Ads",
+            "Search Engines",
+          ],
+        };
+        this.options.yAxis = categoryItem;
+      }
+    },
   },
 };
 </script>
+
+<style scoped>
+.custom-toolbox {
+  position: absolute;
+  right: 2px;
+  top: 30px;
+  cursor: pointer;
+}
+</style>
