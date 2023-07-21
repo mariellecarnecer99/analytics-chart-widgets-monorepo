8709fcddf65c5fa7c91be585b4541dda498b1eba
maynion <mcarnecer@startechup.com>
2023-07-21 14:20:19 +0800

    initial commit: sample socket integration

---
+++ websocket-integration/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<h1>Frontend</h1>
+</template>
+
+<script>
+import SocketioService from "./services/socketio.service.js";
+
+export default {
+	name: "App",
+	components: {},
+	created() {
+		SocketioService.setupSocketConnection();
+	},
+	beforeUnmount() {
+		SocketioService.disconnect();
+	},
+};
+</script>
